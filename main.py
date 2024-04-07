1d3bc318b342a5797a666ffa2dec5f47f55a8c92
pavel_silkin <pashasil-5@mail.ru>
2024-04-07 16:43:54 +0300

    logging_config.yaml

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import logging.config
 
-from logging_settings import logging_config
+import yaml
 from module_1 import main
 
-# Загружаем настройки логирования из словаря `logging_config`
-logging.config.dictConfig(logging_config)
+with open('logging_config.yaml', 'rt') as f:
+    config = yaml.safe_load(f.read())
 
-# Исполняем функцию `main` из модуля `module_1.py`
+logging.config.dictConfig(config)
+
 main()
